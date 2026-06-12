43c2d226cbe5a5677aa0d4330b2bfe977bc245ee
abhi8hero <abhishekugare1289@gmail.com>
2026-06-12 12:41:04 +0530

    Tracker Updates

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,5 +1,5 @@
 export const corsHeaders = {
-  'Access-Control-Allow-Origin': 'https://abhi8hero.github.io/portfolio-abhishek_ugare',
+  'Access-Control-Allow-Origin': 'https://abhi8hero.github.io',
   'Access-Control-Allow-Methods': 'POST, OPTIONS',
   'Access-Control-Allow-Headers':
     'Content-Type, Authorization, x-client-info, apikey',
