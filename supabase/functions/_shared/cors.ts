0301042a933e53971d4b7e75a914183d06035a95
abhi8hero <abhishekugare1289@gmail.com>
2026-06-12 12:07:29 +0530

    Tracker Updates

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,6 +1,7 @@
-// Shared CORS headers for all Edge Functions
 export const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Origin': 'https://abhi8hero.github.io',
   'Access-Control-Allow-Methods': 'POST, OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type, Authorization, x-client-info, apikey',
+  'Access-Control-Allow-Headers':
+    'Content-Type, Authorization, x-client-info, apikey',
+  'Access-Control-Allow-Credentials': 'true',
 };
